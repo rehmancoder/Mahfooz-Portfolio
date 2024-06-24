23b3e7124a46819398d1a9db28e5a21009cdc32f
rehmancoder <rehmanshabbir68@gmail.com>
2024-06-23 22:57:34 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,46 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// // }
+// <div class="flex md:gap-24 gap-14">
+// <ul class="flex flex-col md:gap-5">
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Logo Design</li>
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Branding identity</li>
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Web Design</li>
+// </ul>
+// <ul class="flex flex-col md:gap-5">
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Social Marketing</li>
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Digital Marketing</li>
+//     <li class="text-[#636363] font-semibold flex gap-3"><span><i class="bi bi-arrow-return-right"></i></span> Product Design</li>
+// </ul>
+// </div>
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function arrow(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementById("mySlides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        syne: ["syne","san-serif"],
+        libre: ["Orbitron","san-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
